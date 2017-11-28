b671fc5bf828d09c4c0c9b7774ea5e3c6ca9bace
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-28 17:43:23 +0900

    use `filepath.Join` to generate path

--- osext/osext_test.go
+++ osext/osext_test.go
@@ -2,18 +2,19 @@ package osext
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestIsExit(t *testing.T) {
 	pwd, _ := os.Getwd()
 
-	file := pwd + "/osext_test.go"
+	file := filepath.Join(pwd, "osext_test.go")
 	if !IsExist(file) {
 		t.Errorf("Expect isExist returns true but false. file: %s", file)
 	}
 
-	file = pwd + "/unexist.go"
+	file = filepath.Join(pwd, "unexist.go")
 	if IsExist(file) {
 		t.Errorf("Expect isExist returns false but true. file: %s", file)
 	}
