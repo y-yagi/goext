4370c61dc55a7928ff62d987c292b5c8226ccbf9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-10-14 08:15:14 +0900

    Organize test packages

--- arr/arr_test.go
+++ arr/arr_test.go
@@ -1,21 +1,22 @@
-package arr
+package arr_test
 
 import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/y-yagi/goext/arr"
 )
 
 func TestContains(t *testing.T) {
 	value := "today"
 	list := []string{"today", "week", "month", "all"}
 
-	if !Contains(list, value) {
+	if !arr.Contains(list, value) {
 		t.Errorf("Expect contains returns true but false. list: %v, value: '%s'", list, value)
 	}
 
 	value = "tomorrow"
-	if Contains(list, value) {
+	if arr.Contains(list, value) {
 		t.Errorf("Expect contains returns false but true. list: %v, value: '%s'", list, value)
 	}
 }
@@ -24,7 +25,7 @@ func TestJoin(t *testing.T) {
 	list := []string{"a", "b", "c"}
 	expected := "a-b-c"
 
-	result := Join(list, "-")
+	result := arr.Join(list, "-")
 	if result != expected {
 		t.Errorf("expected %v, but %v.", expected, result)
 	}
@@ -34,7 +35,7 @@ func TestUniqueStrings(t *testing.T) {
 	list := []string{"abc", "bcd", "cde", "abc", "def"}
 	expected := []string{"abc", "bcd", "cde", "def"}
 
-	result := UniqueStrings(list)
+	result := arr.UniqueStrings(list)
 
 	if diff := cmp.Diff(expected, result); diff != "" {
 		t.Errorf("UniqueStrings() mismatch (-want +got):\n%s", diff)
--- timeext/timeext_test.go
+++ timeext/timeext_test.go
@@ -1,13 +1,15 @@
-package timeext
+package timeext_test
 
 import (
 	"testing"
 	"time"
+
+	"github.com/y-yagi/goext/timeext"
 )
 
 func TestBeginningOfMonth(t *testing.T) {
 	date, _ := time.Parse("2006-01-02", "2016-01-10")
-	firstDate := BeginningOfMonth(date)
+	firstDate := timeext.BeginningOfMonth(date)
 
 	if (firstDate.Year() != 2016) || (firstDate.Month() != 1) || (firstDate.Day() != 1) {
 		t.Errorf("Expect beginningOfmonth is 2016-01-01, but %s", firstDate)
@@ -16,14 +18,14 @@ func TestBeginningOfMonth(t *testing.T) {
 
 func TestEndOfMonth(t *testing.T) {
 	date, _ := time.Parse("2006-01-02", "2016-01-10")
-	endDate := EndOfMonth(date)
+	endDate := timeext.EndOfMonth(date)
 
 	if (endDate.Year() != 2016) || (endDate.Month() != 1) || (endDate.Day() != 31) {
 		t.Errorf("Expect endOfMonth is 2016-01-31, but %s", endDate)
 	}
 
 	date, _ = time.Parse("2006-01-02", "2016-02-02")
-	endDate = EndOfMonth(date)
+	endDate = timeext.EndOfMonth(date)
 
 	if (endDate.Year() != 2016) || (endDate.Month() != 2) || (endDate.Day() != 29) {
 		t.Errorf("Expect endOfMonth is 2016-02-29, but %s", endDate)
